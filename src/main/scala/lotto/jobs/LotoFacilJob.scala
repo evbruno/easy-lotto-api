02991fa0be7fa0a504b351eb11d375252dcfa867
Dudu <edu.chaos@gmail.com>
2016-06-08 02:26:23 -0300

    ends

--- src/main/scala/lotto/jobs/LotoFacilJob.scala
+++ src/main/scala/lotto/jobs/LotoFacilJob.scala
@@ -7,7 +7,7 @@ class LotofacilJob(implicit val apiRepo : ApiRepo) extends JobTemplate {
 	private val ENV_KEY: String = "EASY_LOTTO_API_LOTOFACIL_FILE"
 
 	override val downloader =
-		if (sys.env(ENV_KEY) != null) {
+		if (System.getenv(ENV_KEY) != null) {
 			val f = sys.env(ENV_KEY)
 			warn(s"Local file for Lotofacil: $f")
 			new LocalFileDownloader(f)
--- src/main/scala/lotto/jobs/MegaSenaJob.scala
+++ src/main/scala/lotto/jobs/MegaSenaJob.scala
@@ -7,7 +7,7 @@ class MegaSenaJob(implicit val apiRepo : ApiRepo) extends JobTemplate {
 	private val ENV_KEY: String = "EASY_LOTTO_API_MEGA_SENA_FILE"
 
 	override val downloader =
-		if (sys.env(ENV_KEY) != null) {
+		if (System.getenv(ENV_KEY) != null) {
 			val f = sys.env(ENV_KEY)
 			warn(s"Local file for Mega Sena: $f")
 			new LocalFileDownloader(f)
